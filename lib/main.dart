bd9cd6e9350020a346230fdd8a2645a8f340be4c
Bread Dev <114232869+Tushiri@users.noreply.github.com>
2024-01-11 20:03:36 +0800

    random

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,51 @@
 import 'package:emart_app/views/splash_screen/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
 import 'consts/consts.dart';
+import 'package:sellerside_app/services/store_services.dart';
+import 'package:logger/logger.dart';
+
+final Logger logger = Logger();
+
+final storeServices = StoreServices();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  String? deviceToken = await FirebaseMessaging.instance.getToken();
+  logger.w('FCM Device Token: $deviceToken');
+
+  await sendInitialNotification(deviceToken);
+
   runApp(const MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  logger.d("Handling a background message: ${message.messageId}");
+}
+
+Future<void> sendInitialNotification(String? deviceToken) async {
+  if (deviceToken != null) {
+    String? userId = FirebaseAuth.instance.currentUser?.uid;
+
+    if (userId != null) {
+      await storeServices.sendNotification(
+        'Order Status Update',
+        'Your order is now Confirmed',
+        userId,
+        deviceToken,
+      );
+    } else {
+      logger.w('User ID is null. Cannot send initial notification.');
+    }
+  } else {
+    logger.w('Device token is null. Cannot send initial notification.');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -20,11 +59,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.transparent,
         appBarTheme: const AppBarTheme(
-          // to set app bar icons color
           iconTheme: IconThemeData(
             color: darkFontGrey,
           ),
-          //set elevation to 0
           elevation: 0.0,
           backgroundColor: Colors.transparent,
         ),
